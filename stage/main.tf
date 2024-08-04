1f951e9e883a473827d8c347a7f49ab2cc41ca78
Mihai T <contact@mihaiturcu.ro>
2024-08-04 17:47:03 +0300

    Update main.tf

--- stage/main.tf
+++ stage/main.tf
@@ -78,9 +78,9 @@ module "eks" {
 
       instance_types = ["t3.small"]
 
-      min_size     = 1
+      min_size     = 2
       max_size     = 3
-      desired_size = 1
+      desired_size = 2
     }
 
     two = {
